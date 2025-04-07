a7b4c612738480fa47b671eb9731f59f9f15c8d5
Zoe Cox <zoemcox16@gmail.com>
2025-04-07 17:26:17 +0100

    carousel update

--- carousel.js
+++ carousel.js
@@ -3,8 +3,11 @@ const projectText = document.querySelector(".project-text");
 const carouselImg1 = document.querySelector(".carousel-image1");
 const carouselImg2 = document.querySelector(".carousel-image2");
 const carouselImg3 = document.querySelector(".carousel-image3");
+const carouselImg4 = document.querySelector(".carousel-image4");
 
-const carouselImages = [carouselImg1, carouselImg2, carouselImg3];
+const carouselImages = [carouselImg1, carouselImg2, carouselImg3, carouselImg4];
+
+// https://fantasy-demo-game.zoecox.co.uk/
 
 const nextBtn = document.querySelector(".carousel-control-next-icon");
 const prevBtn = document.querySelector(".carousel-control-prev-icon");
@@ -37,11 +40,14 @@ nextBtn.addEventListener("click", () => {
       if (carouselImages[i] === carouselImg3) {
         projectText.innerHTML = "Happy Dog Toys - Dog Toy Web Shop";
       }
+      if (carouselImages[i] === carouselImg4) {
+        projectText.innerHTML = "Fantasy Game Demo - A Short Demo Game";
+      }
     }
   }
 });
 
-https: prevBtn.addEventListener("click", () => {
+prevBtn.addEventListener("click", () => {
   for (let i = carouselImages.length - 1; i >= 0; i--) {
     if (carouselImages[i].classList.contains("active")) {
       if (carouselImages[i] === carouselImg1) {
@@ -53,6 +59,9 @@ https: prevBtn.addEventListener("click", () => {
       if (carouselImages[i] === carouselImg3) {
         console.log("this is image 3");
         projectText.innerHTML = "Princess of Bramboria - Text Adventure Game";
+      }
+      if (carouselImages[i] === carouselImg4) {
+        projectText.innerHTML = "Fantasy Game Demo - A Short Demo Game";
       }
     }
   }
