d58d9a1df919d90d5f36c52cda4c7519546899f9
ZoeCox <zoemcox16@gmail.com>
2025-04-07 18:45:06 +0100

    carousel update

--- carousel.js
+++ carousel.js
@@ -7,8 +7,6 @@ const carouselImg4 = document.querySelector(".carousel-image4");
 
 const carouselImages = [carouselImg1, carouselImg2, carouselImg3, carouselImg4];
 
-// https://fantasy-demo-game.zoecox.co.uk/
-
 const nextBtn = document.querySelector(".carousel-control-next-icon");
 const prevBtn = document.querySelector(".carousel-control-prev-icon");
 
@@ -26,6 +24,9 @@ projectText.addEventListener("click", () => {
   if (projectText.innerHTML === "Table Tennis - Two Player Game") {
     textLinkHandler("https://tennis-game.zoecox.co.uk/");
   }
+  if (projectText.innerHTML === "Fantasy Game Demo - A Short Demo Game") {
+    textLinkHandler("https://fantasy-demo-game.zoecox.co.uk/");
+  }
 });
 
 nextBtn.addEventListener("click", () => {
@@ -38,10 +39,10 @@ nextBtn.addEventListener("click", () => {
         projectText.innerHTML = "Table Tennis - Two Player Game";
       }
       if (carouselImages[i] === carouselImg3) {
-        projectText.innerHTML = "Happy Dog Toys - Dog Toy Web Shop";
+        projectText.innerHTML = "Fantasy Game Demo - A Short Demo Game";
       }
       if (carouselImages[i] === carouselImg4) {
-        projectText.innerHTML = "Fantasy Game Demo - A Short Demo Game";
+        projectText.innerHTML = "Happy Dog Toys - Dog Toy Web Shop";
       }
     }
   }
@@ -51,20 +52,17 @@ prevBtn.addEventListener("click", () => {
   for (let i = carouselImages.length - 1; i >= 0; i--) {
     if (carouselImages[i].classList.contains("active")) {
       if (carouselImages[i] === carouselImg1) {
-        projectText.innerHTML = "Table Tennis - Two Player Game";
+        projectText.innerHTML = "Fantasy Game Demo - A Short Demo Game";
       }
       if (carouselImages[i] === carouselImg2) {
         projectText.innerHTML = "Happy Dog Toys - Dog Toy Web Shop";
       }
       if (carouselImages[i] === carouselImg3) {
-        console.log("this is image 3");
         projectText.innerHTML = "Princess of Bramboria - Text Adventure Game";
       }
       if (carouselImages[i] === carouselImg4) {
-        projectText.innerHTML = "Fantasy Game Demo - A Short Demo Game";
+        projectText.innerHTML = "Table Tennis - Two Player Game";
       }
     }
   }
 });
-
-// projectText.innerHTML = "hello";
